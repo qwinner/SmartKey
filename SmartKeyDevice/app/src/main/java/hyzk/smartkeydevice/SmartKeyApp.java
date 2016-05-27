a5942e8ff5e341cc00eaac6c3e792c7aadeb44f8
qwinner <xiaoqiangaaa2007@163.com>
2016-05-27 16:49:51 +0800

    new app.

---
+++ SmartKeyDevice/app/src/main/java/hyzk/smartkeydevice/SmartKeyApp.java
@@ -0,0 +1,13 @@
+package hyzk.smartkeydevice;
+
+import android.app.Application;
+
+/**
+ * Created by wyq on 2016/5/27.
+ */
+public class SmartKeyApp extends Application {
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
