9a5e593adc5dfdc9af549904b562efdcb55e7a7d
qwinner <xiaoqiangaaa2007@163.com>
2016-05-31 18:06:05 +0800

    add pop windows to finger print.

--- SmartKeyDevice/app/src/main/java/hyzk/smartkeydevice/activity/LoginActivity.java
+++ SmartKeyDevice/app/src/main/java/hyzk/smartkeydevice/activity/LoginActivity.java
@@ -1,14 +1,21 @@
 package hyzk.smartkeydevice.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bigkoo.alertview.AlertView;
+import com.bigkoo.alertview.OnItemClickListener;
 import com.dd.CircularProgressButton;
 
 import hyzk.smartkeydevice.R;
@@ -16,10 +23,12 @@ import android_serialport_api.AsyncFingerprint;
 import android_serialport_api.SerialPortManager;
 import hyzk.smartkeydevice.app.ActivityList;
 import android.fpi.MtGpio;
+import android.widget.Toast;
+
 import hyzk.smartkeydevice.utils.ExtApi;
 import hyzk.smartkeydevice.utils.ToastUtil;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements OnItemClickListener {
 
     private static final int FLAG_HOMEKEY_DISPATCHED = 0x80000000;
 
@@ -37,11 +46,36 @@ public class LoginActivity extends Activity {
     private EditText mEmailView;
     private EditText mPasswordView;
     private CircularProgressButton cpBtn;
+    private Button fingerLogin;
+
+    private AlertView mAlertViewExt;//窗口拓展例子
+    private InputMethodManager imm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         this.getWindow().setFlags(FLAG_HOMEKEY_DISPATCHED, FLAG_HOMEKEY_DISPATCHED);
+
+        InitView();
+        ShowFringerView();
+    }
+
+    private void ShowFringerView(){
+        mAlertViewExt = new AlertView(this.getResources().getString(R.string.finger_login), this.getResources().getString(R.string.finger_readme), this.getResources().getString(R.string.cancel), null, null, this, AlertView.Style.Alert, LoginActivity.this);
+        ViewGroup extView = (ViewGroup) LayoutInflater.from(this).inflate(R.layout.alertext_form,null);
+        mAlertViewExt.addExtView(extView);
+        mAlertViewExt.setMarginBottom(0);
+        mAlertViewExt.show();
+    }
+
+    private void InitView(){
+        fingerLogin = (Button)findViewById(R.id.fingerLogin);
+        fingerLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowFringerView();
+            }
+        });
 
         cpBtn = (CircularProgressButton)findViewById(R.id.btnWithText);
         cpBtn.setProgress(0);
@@ -55,13 +89,17 @@ public class LoginActivity extends Activity {
         });
         mEmailView = (EditText) findViewById(R.id.email);
         mPasswordView = (EditText) findViewById(R.id.password);
+        imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(mEmailView.getWindowToken(),0);
+        imm.hideSoftInputFromWindow(mPasswordView.getWindowToken(), 0);
+       //添加 键盘 位置
 
         ActivityList.getInstance().setMainContext(this);
         ActivityList.getInstance().LoadConfig();
         ActivityList.getInstance().CheckDate="Check";//ExtApi.getDataForID();
         ActivityList.getInstance().DeviceSN= ExtApi.getDeviceID(this);
 
-        mEmailView.setText(ActivityList.getInstance().DefaultUser);
+       // mEmailView.setText(ActivityList.getInstance().DefaultUser);
         ExtApi.CreateDir(ActivityList.getInstance().DirWork);
         ExtApi.CreateDir(ActivityList.getInstance().TmpWork);
         ExtApi.CreateDir(ActivityList.getInstance().CacheWork);
@@ -71,6 +109,24 @@ public class LoginActivity extends Activity {
         MtGpio.getInstance().BCPowerSwitch(false);
     }
 
+
+    @Override
+    public void onItemClick(Object o,int position) {
+        //closeKeyboard();
+        //判断是否是拓展窗口View，而且点击的是非取消按钮
+//        if(o == mAlertViewExt && position != AlertView.CANCELPOSITION){
+//            String name = etName.getText().toString();
+//            if(name.isEmpty()){
+//                Toast.makeText(this, "啥都没填呢", Toast.LENGTH_SHORT).show();
+//            }
+//            else{
+//                Toast.makeText(this, "hello,"+name, Toast.LENGTH_SHORT).show();
+//            }
+//
+//            return;
+//        }
+
+    }
 
     private void LoginSuccess(){
         new Thread(new Runnable() {
